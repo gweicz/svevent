e64f05dc84226b563145374b8eccebf72773baff
tree <tree@hidden.cz>
2023-02-12 23:02:30 +0100

    first commit

---
+++ src/lib/components/PeopleList.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let items;
+	export let people;
+    export let size = 'normal';
+
+	function getPerson(id) {
+		return people.find((p) => p.id === id);
+	}
+	function twitterLink(handle) {
+		return `https://twitter.com/${handle}`;
+	}
+</script>
+
+{#each items.map(getPerson) as item}
+	<div class="hover:bg-white hover:text-black p-2 {size === 'small' ? 'w-48' : 'w-64'} person-item">
+		<div>
+			<a href={twitterLink(item.twitter)} target="_blank"
+				><img src="/people/{item.img}" class="grayscale invert aspect-square object-cover" /></a
+			>
+		</div>
+		<div class="mt-4">{item.name}</div>
+		<div class="text-base text-mild">
+			<a href={twitterLink(item.twitter)} class="hover:underline">@{item.twitter}</a>
+		</div>
+	</div>
+{/each}
